f2f0ddb44f4c3536ec16d5068ef37dd4b770b778
Russell Muller <Bigjoos1@gmail.com>
2011-12-14 18:36:10 +0000

    Old header

--- browse.php
+++ browse.php
@@ -1,10 +1,10 @@
 <?php
 /**
- *   https://09source.kicks-ass.net:8443/svn/installer09/
+ *   https://github.com/Bigjoos/
  *   Licence Info: GPL
- *   Copyright (C) 2010 U-232 v.2
+ *   Copyright (C) 2010 U-232 v.3
  *   A bittorrent tracker source based on TBDev.net/tbsource/bytemonsoon.
- *   Project Leaders: Mindless,putyn,kidvision.
+ *   Project Leaders: Mindless, putyn.
  **/
 require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'include'.DIRECTORY_SEPARATOR.'bittorrent.php');
 require_once(INCL_DIR.'user_functions.php');
