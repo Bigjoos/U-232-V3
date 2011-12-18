4da439fd783d0a260c4d4136d54ad218353748aa
Russell Muller <Bigjoos1@gmail.com>
2011-12-18 11:57:30 +0000

    Set html_strip false for show more link

--- include/torrenttable_functions.php
+++ include/torrenttable_functions.php
@@ -307,7 +307,7 @@ if ($oldlink > 0)
        }
     
        $htmlout .= "</tr>\n";
-       $htmlout .="<tr id=\"kdescr{$row["id"]}\" style=\"display:none;\"><td width=\"100%\" colspan=\"13\">".format_comment($descr)."</td></tr>\n";
+       $htmlout .="<tr id=\"kdescr{$row["id"]}\" style=\"display:none;\"><td width=\"100%\" colspan=\"13\">".format_comment($descr, false)."</td></tr>\n";
        }
        $htmlout .= "</table><!--</div>-->\n";
        return $htmlout;
