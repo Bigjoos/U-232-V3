dae8e59e8ba1f14a2d9c042ec0f7436f74b506b6
Russell Muller <Bigjoos1@gmail.com>
2011-12-18 13:11:57 +0000

    Updates

--- blocks/index/latest_user.php
+++ blocks/index/latest_user.php
@@ -2,7 +2,7 @@
 /** latestuser index **/
     $latestuser_cache = $mc1->get_value('latestuser');
     if ($latestuser_cache === false) {
-    $latestuser_cache = mysqli_fetch_assoc(@sql_query('SELECT id, username FROM users WHERE status="confirmed" ORDER BY id DESC LIMIT 1'))/* or sqlerr(__FILE__, __LINE__)*/;
+    $latestuser_cache = mysqli_fetch_assoc(sql_query('SELECT id, username, class, warned, chatpost, pirate, king, leechwarn, enabled, donor FROM users WHERE status="confirmed" ORDER BY id DESC LIMIT 1'))/* or sqlerr(__FILE__, __LINE__)*/;
     $latestuser_cache['id']  = (int)$latestuser_cache['id']; // so is stored as an integer
     /** OOP **/
     $mc1->cache_value('latestuser', $latestuser_cache, $INSTALLER09['expires']['latestuser']);
@@ -10,7 +10,7 @@
     $latestuser = '<div class="headline">Latest Member</div>
     <div class="headbody">
     Welcome to our newest member 
-    <b><a href="userdetails.php?id='.$latestuser_cache['id'].'">'.$latestuser_cache['username'].'</a></b>&nbsp;!
+    <b><a href="userdetails.php?id='.$latestuser_cache['id'].'">'.format_username($latestuser_cache).'</a>!</b>
     </div><br />';
 //==MemCached latest user
 $HTMLOUT .= $latestuser;
