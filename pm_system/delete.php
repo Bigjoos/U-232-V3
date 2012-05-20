7cfbf36f53b53ff4730904ac611a09416c35a303
Russell Muller <Bigjoos1@gmail.com>
2012-05-20 12:41:02 +0000

    Edit.

--- pm_system/delete.php
+++ pm_system/delete.php
@@ -28,20 +28,20 @@ if (!defined('BUNNY_PM_SYSTEM'))
         if ($message['receiver'] == $CURUSER['id']/* && $message['saved'] == 'no'*/ || $message['sender'] == $CURUSER['id'] && $message['location'] == PM_DELETED)
             {
             sql_query('DELETE FROM messages WHERE id='.sqlesc($pm_id)) or sqlerr(__FILE__,__LINE__);
-            $mc1->delete_value('inbox_new_'.$receiver);
-            $mc1->delete_value('inbox_new_sb_'.$receiver);
+            $mc1->delete_value('inbox_new_'.$pm_id);
+            $mc1->delete_value('inbox_new_sb_'.$pm_id);
             }
         elseif ($message['receiver'] == $CURUSER['id']/* && $message['saved'] == 'yes'*/)
             {
             sql_query('UPDATE messages SET location=0, unread=\'no\' WHERE id='.sqlesc($pm_id)) or sqlerr(__FILE__,__LINE__);
-            $mc1->delete_value('inbox_new_'.$receiver);
-            $mc1->delete_value('inbox_new_sb_'.$receiver);
+            $mc1->delete_value('inbox_new_'.$pm_id);
+            $mc1->delete_value('inbox_new_sb_'.$pm_id);
             }
         elseif ($message['sender'] == $CURUSER['id'] && $message['location'] != PM_DELETED)
             {
             sql_query('UPDATE messages SET saved=\'no\' WHERE id='.sqlesc($id)) or sqlerr(__FILE__,__LINE__);
-            $mc1->delete_value('inbox_new_'.$receiver);
-            $mc1->delete_value('inbox_new_sb_'.$receiver);
+            $mc1->delete_value('inbox_new_'.$id);
+            $mc1->delete_value('inbox_new_sb_'.$id);
             }
 
         //=== see if it worked :D
