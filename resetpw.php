59e7e0538d02d2f3ec8e8d15d21f1b99f04a41b9
Russell Muller <Bigjoos1@gmail.com>
2013-01-12 15:40:47 +0000

    Updated.

--- resetpw.php
+++ resetpw.php
@@ -7,9 +7,9 @@
  *   Project Leaders: Mindless, putyn.
  *
  */
-require_once (dirname(__FILE__).DIRECTORY_SEPARATOR.'include'.DIRECTORY_SEPARATOR.'bittorrent.php');
-require_once (INCL_DIR.'user_functions.php');
-require_once (INCL_DIR.'password_functions.php');
+require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'bittorrent.php');
+require_once (INCL_DIR . 'user_functions.php');
+require_once (INCL_DIR . 'password_functions.php');
 dbconn();
 ini_set('session.use_trans_sid', '0');
 session_start();
@@ -31,7 +31,7 @@ if ($CURUSER) stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error1']}");
 $step = (isset($_GET["step"]) ? (int)$_GET["step"] : (isset($_POST["step"]) ? (int)$_POST["step"] : ''));
 if ($step == '1') {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (!mkglobal('email'.($INSTALLER09['captcha_on'] ? ":captchaSelection" : "").'')) stderr("Oops", "Missing form data - You must fill all fields");
+        if (!mkglobal('email' . ($INSTALLER09['captcha_on'] ? ":captchaSelection" : "") . '')) stderr("Oops", "Missing form data - You must fill all fields");
         if ($INSTALLER09['captcha_on']) {
             if (empty($captchaSelection) || $_SESSION['simpleCaptchaAnswer'] != $captchaSelection) {
                 stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error2']}");
@@ -40,7 +40,7 @@ if ($step == '1') {
         }
         if (empty($email)) stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_invalidemail']}");
         if (!validemail($email)) stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_invalidemail1']}");
-        $check = sql_query('SELECT id, status, passhint, hintanswer FROM users WHERE email = '.sqlesc($email)) or sqlerr(__FILE__, __LINE__);
+        $check = sql_query('SELECT id, status, passhint, hintanswer FROM users WHERE email = ' . sqlesc($email)) or sqlerr(__FILE__, __LINE__);
         $assoc = mysqli_fetch_assoc($check) or stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_notfound']}");
         if (empty($assoc['passhint']) || empty($assoc['hintanswer'])) {
             stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error3']}");
@@ -48,7 +48,7 @@ if ($step == '1') {
         if ($assoc['status'] != 'confirmed') {
             stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error4']}");
         } else {
-            $HTMLOUT.= "<form method='post' action='".$_SERVER['PHP_SELF']."?step=2'>
+            $HTMLOUT.= "<form method='post' action='" . $_SERVER['PHP_SELF'] . "?step=2'>
 <table border='1' cellspacing='0' cellpadding='10'>
 <tr>
 <td class='rowhead'>{$lang['main_question']}</td>";
@@ -66,25 +66,25 @@ if ($step == '1') {
             $question[6] = "{$lang['main_question6']}";
             $passhint = preg_replace($id, $question, (int)$assoc['passhint']);
             $HTMLOUT.= "<td><i><b>{$passhint} ?</b></i>
-<input type='hidden' name='id' value='".(int)$assoc['id']."' /></td></tr>
+<input type='hidden' name='id' value='" . (int)$assoc['id'] . "' /></td></tr>
 <tr><td class='rowhead'>{$lang['main_sec_answer']}</td>
 <td><input type='text' size='40' name='answer' /></td></tr><tr><td colspan='2' align='center'><input type='submit' value='{$lang['main_next']}' class='btn' />
 </td></tr></table></form>";
-            echo stdhead('Reset Lost Password').$HTMLOUT.stdfoot();
+            echo stdhead('Reset Lost Password') . $HTMLOUT . stdfoot();
         }
     }
 } elseif ($step == '2') {
     if (!mkglobal('id:answer')) die();
-    $select = sql_query('SELECT id, username, hintanswer FROM users WHERE id = '.sqlesc($id)) or sqlerr(__FILE__, __LINE__);
+    $select = sql_query('SELECT id, username, hintanswer FROM users WHERE id = ' . sqlesc($id)) or sqlerr(__FILE__, __LINE__);
     $fetch = mysqli_fetch_assoc($select);
     if (!$fetch) stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error5']}");
     if (empty($answer)) stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error6']}");
     if ($fetch['hintanswer'] != md5($answer)) {
         $ip = getip();
         $useragent = $_SERVER['HTTP_USER_AGENT'];
-        $msg = "".htmlspecialchars($fetch['username']).", on ".get_date(TIME_NOW, '', 1, 0).", {$lang['main_message']}"."\n\n{$lang['main_message1']} ".$ip." (".@gethostbyaddr($ip).")"."\n {$lang['main_message2']} ".$useragent."\n\n {$lang['main_message3']}\n {$lang['main_message4']}\n";
+        $msg = "" . htmlspecialchars($fetch['username']) . ", on " . get_date(TIME_NOW, '', 1, 0) . ", {$lang['main_message']}" . "\n\n{$lang['main_message1']} " . $ip . " (" . @gethostbyaddr($ip) . ")" . "\n {$lang['main_message2']} " . $useragent . "\n\n {$lang['main_message3']}\n {$lang['main_message4']}\n";
         $subject = "Failed password reset";
-        sql_query('INSERT INTO messages (receiver, msg, subject, added) VALUES ('.sqlesc((int)$fetch['id']).', '.sqlesc($msg).', '.sqlesc($subject).', '.TIME_NOW.')') or sqlerr(__FILE__, __LINE__);
+        sql_query('INSERT INTO messages (receiver, msg, subject, added) VALUES (' . sqlesc((int)$fetch['id']) . ', ' . sqlesc($msg) . ', ' . sqlesc($subject) . ', ' . TIME_NOW . ')') or sqlerr(__FILE__, __LINE__);
         stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error7']}");
     } else {
         $HTMLOUT.= "<form method='post' action='?step=3'>
@@ -93,12 +93,16 @@ if ($step == '1') {
 <td><input type='password' size='40' name='newpass' /></td></tr>
 <tr><td class='rowhead'>{$lang['main_new_pass_confirm']}</td><td><input type='password' size='40' name='newpassagain' /></td></tr>
 <tr><td colspan='2' align='center'><input type='submit' value='{$lang['main_changeit']}' class='btn' />
-<input type='hidden' name='id' value='".(int)$fetch['id']."' /></td></tr></table></form>";
-        echo stdhead('Reset Lost Password').$HTMLOUT.stdfoot();
+<input type='hidden' name='id' value='" . (int)$fetch['id'] . "' /></td></tr></table></form>";
+        echo stdhead('Reset Lost Password') . $HTMLOUT . stdfoot();
     }
 } elseif ($step == '3') {
     if (!mkglobal('id:newpass:newpassagain')) die();
-    $select = sql_query('SELECT id, editsecret FROM users WHERE id = '.sqlesc($id)) or sqlerr(__FILE__, __LINE__);
+    if ($assoc['id'] != $fetch['id']) {
+        stderr('Pmsl', 'Run along now - nothing to see here');
+        exit();
+    }
+    $select = sql_query('SELECT id, editsecret FROM users WHERE id = ' . sqlesc($id)) or sqlerr(__FILE__, __LINE__);
     $fetch = mysqli_fetch_assoc($select) or stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error8']}");
     if (empty($newpass)) stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error9']}");
     if ($newpass != $newpassagain) stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error10']}");
@@ -106,15 +110,15 @@ if ($step == '1') {
     if (strlen($newpass) > 40) stderr("{$lang['stderr_errorhead']}", "{$lang['stderr_error12']}");
     $secret = mksecret();
     $newpassword = make_passhash($secret, md5($newpass));
-    sql_query('UPDATE users SET secret = '.sqlesc($secret).', editsecret = "", passhash='.sqlesc($newpassword).' WHERE id = '.sqlesc($id).' AND editsecret = '.sqlesc($fetch["editsecret"]));
-    $mc1->begin_transaction('MyUser_'.$id);
+    sql_query('UPDATE users SET secret = ' . sqlesc($secret) . ', editsecret = "", passhash=' . sqlesc($newpassword) . ' WHERE id = ' . sqlesc($id) . ' AND editsecret = ' . sqlesc($fetch["editsecret"]));
+    $mc1->begin_transaction('MyUser_' . $id);
     $mc1->update_row(false, array(
         'secret' => $secret,
         'editsecret' => '',
         'passhash' => $newpassword
     ));
     $mc1->commit_transaction($INSTALLER09['expires']['curuser']);
-    $mc1->begin_transaction('user'.$id);
+    $mc1->begin_transaction('user' . $id);
     $mc1->update_row(false, array(
         'secret' => $secret,
         'editsecret' => '',
@@ -134,15 +138,15 @@ if ($step == '1') {
     </script>
 <p>{$lang['main_body']}</p>
 <br />
-<form method='post' action='".$_SERVER['PHP_SELF']."?step=1'>
+<form method='post' action='" . $_SERVER['PHP_SELF'] . "?step=1'>
 <table border='1' cellspacing='0' cellpadding='10'>
 <tr>
 <td class='rowhead'>{$lang['main_email_add']}</td><td><input type='text' size='40' name='email' /></td></tr>
 <tr>
-".($INSTALLER09['captcha_on'] ? "<td class='rowhead' colspan='2' id='captchareset'></td>" : "")."
+" . ($INSTALLER09['captcha_on'] ? "<td class='rowhead' colspan='2' id='captchareset'></td>" : "") . "
 </tr>
 <tr><td colspan='2' align='center'><input type='submit' value='{$lang['main_recover']}' style='height: 25px' /></td></tr></table>
 </form>";
-    echo stdhead('Reset Lost Password', true, $stdhead).$HTMLOUT.stdfoot();
+    echo stdhead('Reset Lost Password', true, $stdhead) . $HTMLOUT . stdfoot();
 }
 ?>
