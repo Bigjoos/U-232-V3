302ec219416c2a3a3afb5b94d692c6642a94de88
Russell Muller <Bigjoos1@gmail.com>
2013-02-07 21:27:53 +0000

    Update.

--- blackjack.php
+++ blackjack.php
@@ -63,7 +63,7 @@ if ($game) {
             if ($CURUSER["downloaded"] > 0) $ratio = number_format($CURUSER["uploaded"] / $CURUSER["downloaded"], 3);
             elseif ($CURUSER["uploaded"] > 0) $ratio = 999;
             else $ratio = 0;
-            if ($ratio < $required_ratio) stderr("Sorry " . $CURUSER["username"], "Your ratio is lower than the requirement of " . $required_ratio . "%.");
+            if ($INSTALLER09['ratio_free'] === false && $ratio < $required_ratio) stderr("Sorry " . $CURUSER["username"], "Your ratio is lower than the requirement of " . $required_ratio . "%.");
             $res = sql_query("SELECT status, gameover FROM blackjack WHERE userid = " . sqlesc($CURUSER['id']));
             $arr = mysqli_fetch_assoc($res);
             if ($arr['status'] == 'waiting') stderr("Sorry", "You'll have to wait until your last game completes before you play a new one.");
